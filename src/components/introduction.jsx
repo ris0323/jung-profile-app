a9d762d161e9f7de1a891b8542c3ee1cc084b45e
ris0323 <s1079s1079@gmail.com>
2024-07-03 22:58:39 +0800

    first commit

---
+++ src/components/introduction.jsx
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{ backgroundImage: 'url(images/new1.jpg)', backgroundPosition:50}} >
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />Welcome my Profile</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/14wWpF9NDG4bSBE-oML8USW0tjylCeISf/view?usp=drive_link" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: 'url(images/img6.jpg)',backgroundPosition:50 }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I love building<br /> THINGS !!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/ris0323" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: 'url(images/img5.jpg)',backgroundPosition:50 }}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>You're never a loser <br />until you quit trying. </h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://www.youtube.com/channel/UCIOhbFFiGIWX7Vdjpo5T8BQ" target="_blank" rel="noopener noreferrer">View Youtube <i className="icon-youtube" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
