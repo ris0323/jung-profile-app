788f65feade85315de5ecf02b0c5e6821b8c2e70
ris0323 <s1079s1079@gmail.com>
2024-07-04 00:00:22 +0800

    first commit

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,7 +7,7 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{ backgroundImage: 'url(images/new1.jpg)', backgroundPosition:50}} >
+            <li style={{ backgroundColor:'#000000'}} >
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -15,14 +15,14 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />Welcome my Profile</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/14wWpF9NDG4bSBE-oML8USW0tjylCeISf/view?usp=drive_link" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/14wWpF9NDG4bSBE-oML8USW0tjylCeISf/view?usp=drive_link" target="_blank" rel="noopener noreferrer"><mark>View CV</mark><i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{ backgroundImage: 'url(images/img6.jpg)',backgroundPosition:50 }}>
+              <li style={{ backgroundColor:'#000000' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -30,14 +30,14 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/ris0323" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/ris0323" target="_blank" rel="noopener noreferrer"><mark>View Projects </mark><i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{ backgroundImage: 'url(images/img5.jpg)',backgroundPosition:50 }}>
+              <li style={{ backgroundColor:'#000000' }}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -45,7 +45,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1>You're never a loser <br />until you quit trying. </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://www.youtube.com/channel/UCIOhbFFiGIWX7Vdjpo5T8BQ" target="_blank" rel="noopener noreferrer">View Youtube <i className="icon-youtube" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://www.youtube.com/channel/UCIOhbFFiGIWX7Vdjpo5T8BQ" target="_blank" rel="noopener noreferrer"><mark>View Youtube </mark><i className="icon-youtube" /></a></p>
                         </div>
                       </div>
                     </div>
